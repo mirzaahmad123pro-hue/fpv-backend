c4a9a0c3ee87da2958a17fc65588903fee4099ba
mirzaahmad123pro-hue <mirzaahmad123pro@gmail.com>
2026-08-30 14:09:58 +0500

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const { authenticate } = require('../middleware/authMiddleware');
-const admin = require('../middleware/adminMiddleware');
+const { requireAdmin } = require('../middleware/adminMiddleware');
+
 router.route('/')
   .get(productController.getProducts)
-.post(authenticate, admin, productController.createProduct);
+  .post(authenticate, requireAdmin, productController.createProduct);
 
 router.route('/:slug')
   .get(productController.getProductBySlug);
 
 router.route('/:id')
-.put(authenticate, admin, productController.updateProduct)
-.delete(authenticate, admin, productController.deleteProduct);
+  .put(authenticate, requireAdmin, productController.updateProduct)
+  .delete(authenticate, requireAdmin, productController.deleteProduct);
 
 module.exports = router;
