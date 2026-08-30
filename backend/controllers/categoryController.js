fecd1fa71d3eefafc0ff56f9bd48fc5ff6e78a99
mirzaahmad123pro-hue <mirzaahmad123pro@gmail.com>
2026-08-30 14:40:36 +0500

    Update categoryController.js

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,4 +1,4 @@
-const { pool } = require('../config/database');
+const pool  = require('../config/database');
 const { asyncHandler } = require('../middleware/errorMiddleware');
 const { slugify } = require('../utils/helpers');
 
