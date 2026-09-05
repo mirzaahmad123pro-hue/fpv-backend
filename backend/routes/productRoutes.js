b76001473c9384ff89304058038e087eefac68ba
mirzaahmad123pro-hue <mirzaahmad123pro@gmail.com>
2026-09-05 20:01:10 +0500

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const { uploadProductImages } = require('../middleware/uploadMiddleware');
 
 router.get('/', productController.getProducts);
 router.get('/id/:id', productController.getProductById);
 router.get('/slug/:slug', productController.getProductBySlug);
 router.get('/:id', productController.getProductById);
 
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
+// uploadProductImages.array('images', 8) lganay se FormData ke text fields aur images parse hon gi
+router.post('/', uploadProductImages.array('images', 8), productController.createProduct);
+router.put('/:id', uploadProductImages.array('images', 8), productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
